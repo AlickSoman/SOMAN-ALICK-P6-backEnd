2a734b8c7679ee91908acbf16c92d499ef3447db
Alick SOMAN <alick.soman@hotmail.fr>
2021-10-15 17:02:36 +0200

    protect db

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 //=====================================================conexion BD
 
 mongoose.connect(
-    'mongodb://User001:user001@clusterp6oc-shard-00-00.hgacb.mongodb.net:27017,clusterp6oc-shard-00-01.hgacb.mongodb.net:27017,clusterp6oc-shard-00-02.hgacb.mongodb.net:27017/dbP6OC?ssl=true&replicaSet=atlas-64qh95-shard-0&authSource=admin&retryWrites=true&w=majority',
+   
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !!"))
